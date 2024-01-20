14d420048ef9f66f65e49043c67107ae1ff7bee2
milselarch <milselarch@gmail.com>
2024-01-20 23:20:13 +0800

    add health callbacks

--- Assets/ScriptableObjects/IntVariable.cs
+++ Assets/ScriptableObjects/IntVariable.cs
@@ -1,59 +1,69 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "IntVariable", menuName = "ScriptableObjects/IntVariable", order = 2)]
 public class IntVariable: Variable<int> {
     public int previousHighestValue;
-    public override void SetValue(int value)
-    {
+    public delegate void ValueChangeCallback(int prevValue, int newValue);
+    private readonly List<ValueChangeCallback> _callbacks = new();
+
+    public override void SetValue(int value) {
+        var prevValue = this.Value;
         if (value > previousHighestValue) {
             previousHighestValue = value;
         }
 
         _value = value;
+        TriggerCallbacks(prevValue, _value);
+    }
+
+    private void TriggerCallbacks(int prevValue, int newValue) {
+        if (prevValue != newValue) { return; }
+        foreach (var callback in _callbacks) {
+            callback(prevValue, newValue);
+        }
+    }
+
+    public void AttachCallback(ValueChangeCallback callback) {
+        _callbacks.Add(callback);
     }
 
     // overload
-    public void SetValue(IntVariable value)
-    {
+    public void SetValue(IntVariable value) {
         SetValue(value.Value);
     }
 
-    public void Increment()
-    {
+    public void Increment() {
         this.ApplyChange(1);
     }
     
-    public void Increment(int amount, int ceiling)
-    {
-        int newValue = Math.Min(ceiling, this.Value + amount);
+    public void Increment(int amount, int ceiling) {
+        var newValue = Math.Min(ceiling, this.Value + amount);
         this.SetValue(newValue);
     }
 
-    public void Decrement(int amount, int floor)
-    {
-        int newValue = Math.Max(floor, this.Value - amount);
+    public void Decrement(int amount, int floor) {
+        var newValue = Math.Max(floor, this.Value - amount);
         this.SetValue(newValue);
     }
 
-    public void ApplyChange(int amount)
-    {
+    public void ApplyChange(int amount) {
+        var prevValue = this.Value;
         this.Value += amount;
+        TriggerCallbacks(prevValue, _value);
     }
 
-    public void ApplyChange(int amount, int ceilingValue)
-    {
-        int newValue = Math.Min(ceilingValue, this.Value + amount);
+    public void ApplyChange(int amount, int ceilingValue) {
+        var newValue = Math.Min(ceilingValue, this.Value + amount);
         this.SetValue(newValue);
     }
 
-    public void ApplyChange(IntVariable amount)
-    {
+    public void ApplyChange(IntVariable amount) {
         ApplyChange(amount.Value);
     }
 
-    public void ResetHighestValue()
-    {
+    public void ResetHighestValue() {
         previousHighestValue = 0;
     }
 
