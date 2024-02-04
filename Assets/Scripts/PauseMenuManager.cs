412ecf60125395e7661f66eff8837ec336ce92eb
milselarch <milselarch@gmail.com>
2024-02-05 00:32:45 +0800

    add restart button in pause menu

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
@@ -12,18 +13,27 @@ using Cursor = UnityEngine.Cursor;
 public class PauseMenuManager : MonoBehaviour {
     public GameObject pauseCanvas;
     public UIDocument pauseUI;
-    
+    public UnityEvent restartEvent;
+
     private Button _playButton;
     private Button _manuMenuButton;
+    private Button _restartButton;
 
     private void Initialize() {
         var root = pauseUI.rootVisualElement;
         this._playButton = root.Q<Button>("PlayButton");
         this._manuMenuButton = root.Q<Button>("MainMenuButton");
+        this._restartButton = root.Q<Button>("RestartButton");
             
         Assert.IsNotNull(this._playButton);
         this._playButton.clickable.clicked += OnPlayButtonClicked;
         this._manuMenuButton.clickable.clicked += OnMainMenuButtonClicked;
+        this._restartButton.clickable.clicked += OnRestartButtonClicked;
+    }
+    
+    private void OnRestartButtonClicked() {
+        restartEvent.Invoke();
+        Hide();
     }
     
     private void Show() {
